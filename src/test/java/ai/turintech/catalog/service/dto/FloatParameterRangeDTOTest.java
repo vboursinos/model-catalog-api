144ea63021fe7309bd3c68ad48b245d358334c6a
v.boursinos <v.boursinos@turintech.ai>
2023-10-17 14:59:22 +0100

    test: delete incorrect tests

--- src/test/java/ai/turintech/catalog/service/dto/FloatParameterRangeDTOTest.java
+++ src/test/java/ai/turintech/catalog/service/dto/FloatParameterRangeDTOTest.java
@@ -5,18 +5,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import ai.turintech.catalog.web.rest.TestUtil;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 class FloatParameterRangeDTOTest {
 
     @Test
     void dtoEqualsVerifier() throws Exception {
         TestUtil.equalsVerifier(FloatParameterRangeDTO.class);
         FloatParameterRangeDTO floatParameterRangeDTO1 = new FloatParameterRangeDTO();
-        floatParameterRangeDTO1.setId(1L);
+        floatParameterRangeDTO1.setId(UUID.fromString("1L"));
         FloatParameterRangeDTO floatParameterRangeDTO2 = new FloatParameterRangeDTO();
         assertThat(floatParameterRangeDTO1).isNotEqualTo(floatParameterRangeDTO2);
         floatParameterRangeDTO2.setId(floatParameterRangeDTO1.getId());
         assertThat(floatParameterRangeDTO1).isEqualTo(floatParameterRangeDTO2);
-        floatParameterRangeDTO2.setId(2L);
+        floatParameterRangeDTO2.setId(UUID.fromString("2L"));
         assertThat(floatParameterRangeDTO1).isNotEqualTo(floatParameterRangeDTO2);
         floatParameterRangeDTO1.setId(null);
         assertThat(floatParameterRangeDTO1).isNotEqualTo(floatParameterRangeDTO2);
